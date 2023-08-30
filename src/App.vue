8a11d598521698963d8f34ee8e84c21a45253968
pss940909 <pss940909@gmail.com>
2023-08-30 13:29:00 +0800

    first commit & add learning res cmp

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="item in storedResources"
+      :key="item.id"
+      :title="item.title"
+      :des="item.description"
+      :resLink="item.link"
+    >
+    </learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resource/LearningResource.vue';
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official',
+          title: 'The Offcial Guide',
+          description: 'The officail Vue.js documentation',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 'google',
+          title: 'Google.com',
+          description: 'Learn to google...',
+          link: 'https://www.google.com.tw/?hl=zh_TW',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/learning-resource/LearningResource.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ des }}</p>
+    <nav>
+      <a :href="resLink">View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'des', 'resLink'],
+};
+</script>
